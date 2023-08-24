00f10a33bf0be6968cd48225f7f45a8cc261d536
Rohan Paranjpe <48198937+rohanvp@users.noreply.github.com>
2023-08-24 18:48:57 -0400

    Added db file

---
+++ db/portfolio-website-builder.sql
@@ -0,0 +1,208 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: Aug 24, 2023 at 10:48 PM
+-- Server version: 5.7.39
+-- PHP Version: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `portfolio-website-builder`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `about_me`
+--
+
+CREATE TABLE `about_me` (
+  `c1` varchar(25) NOT NULL,
+  `c2` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `about_me`
+--
+
+INSERT INTO `about_me` (`c1`, `c2`) VALUES
+('background', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'),
+('skills', 'skill1,skill2,skill3,skill4');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `experience`
+--
+
+CREATE TABLE `experience` (
+  `experience_order` int(11) NOT NULL,
+  `experience_title` varchar(100) NOT NULL,
+  `experience_org` text NOT NULL,
+  `experience_skills` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `experience`
+--
+
+INSERT INTO `experience` (`experience_order`, `experience_title`, `experience_org`, `experience_skills`) VALUES
+(1, 'Experience Title 1', 'Organization 1', 'Skill1,Skill2,Skill3'),
+(2, 'Experience Title 2', 'Organization 2', 'Skill1,Skill2,Skill3');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `home_section`
+--
+
+CREATE TABLE `home_section` (
+  `c1` varchar(25) NOT NULL,
+  `c2` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `home_section`
+--
+
+INSERT INTO `home_section` (`c1`, `c2`) VALUES
+('home-section-description', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pretium nibh ipsum consequat nisl vel pretium lectus quam id.');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `meta_details`
+--
+
+CREATE TABLE `meta_details` (
+  `c1` varchar(25) NOT NULL,
+  `c2` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `meta_details`
+--
+
+INSERT INTO `meta_details` (`c1`, `c2`) VALUES
+('footer_address', 'Address Here'),
+('footer_description', 'Description Here'),
+('footer_email', 'Email Here'),
+('github_link', 'github link here'),
+('linkedin_link', 'linkedin link here'),
+('meta-description', 'Description meta tag in header.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pretium nibh ipsum consequat nisl vel pretium lectus quam id.'),
+('personal_website_link', 'personal-website-link here'),
+('user-name', 'Your Name');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `projects`
+--
+
+CREATE TABLE `projects` (
+  `project_index` int(11) NOT NULL,
+  `project_title` text NOT NULL,
+  `project_description` text NOT NULL,
+  `project_links` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `projects`
+--
+
+INSERT INTO `projects` (`project_index`, `project_title`, `project_description`, `project_links`) VALUES
+(1, 'Project 1', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pretium nibh ipsum consequat nisl vel pretium lectus quam id.', 'link1;link2'),
+(2, 'Project 2', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pretium nibh ipsum consequat nisl vel pretium lectus quam id.', 'link1;link2'),
+(3, 'Project 3', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pretium nibh ipsum consequat nisl vel pretium lectus quam id.', 'link1;link2'),
+(4, 'Project 4', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pretium nibh ipsum consequat nisl vel pretium lectus quam id.', 'link1;link2'),
+(5, 'Project 5', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pretium nibh ipsum consequat nisl vel pretium lectus quam id.', 'link1;link2'),
+(6, 'Project 6', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pretium nibh ipsum consequat nisl vel pretium lectus quam id.', 'link1;link2');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `section_details`
+--
+
+CREATE TABLE `section_details` (
+  `section_name` varchar(20) NOT NULL,
+  `section_order` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `section_details`
+--
+
+INSERT INTO `section_details` (`section_name`, `section_order`) VALUES
+('abc', -1),
+('about', 2),
+('experience', 4),
+('footer', 6),
+('hero', 1),
+('projects', 3);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `about_me`
+--
+ALTER TABLE `about_me`
+  ADD PRIMARY KEY (`c1`);
+
+--
+-- Indexes for table `experience`
+--
+ALTER TABLE `experience`
+  ADD PRIMARY KEY (`experience_order`);
+
+--
+-- Indexes for table `home_section`
+--
+ALTER TABLE `home_section`
+  ADD PRIMARY KEY (`c1`);
+
+--
+-- Indexes for table `meta_details`
+--
+ALTER TABLE `meta_details`
+  ADD PRIMARY KEY (`c1`);
+
+--
+-- Indexes for table `projects`
+--
+ALTER TABLE `projects`
+  ADD PRIMARY KEY (`project_index`);
+
+--
+-- Indexes for table `section_details`
+--
+ALTER TABLE `section_details`
+  ADD PRIMARY KEY (`section_name`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `projects`
+--
+ALTER TABLE `projects`
+  MODIFY `project_index` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
